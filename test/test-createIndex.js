39a98daf6e025f4ccbff6537aae6ed612f19e258
Alper Özışık <alper.ozisik@smartface.io>
2016-01-20 09:44:26 +0000

    Updated test scripts and sorted output

--- test/test-createIndex.js
+++ test/test-createIndex.js
@@ -1,6 +1,6 @@
 var Workspace = require("../lib/workspace");
 var Device = require("../lib/device");
-
+var argv = process.argv.slice(2);
 var ws = new Workspace({
     path: "/home/ubuntu/workspace/test/workspace",
     projectID: "https://someurl.com"
@@ -9,24 +9,28 @@ var ws = new Workspace({
 //var projectID = ws.getProjectID(true);
 var start = new Date(),
     end;
-/*ws.getIndex(new Device({
-    os: "iOS",
-    brandName: "iPhone 6S"
-}), function indexResult(index) {
-    end = new Date();
-    console.log("Performance is: " + Number(end - start) + "ms.");
-    console.log(index);
-});*/
 
-ws.getIndex(new Device({
-    os: "Android",
-    brandName: "Note 3",
-    resourceFolderOrder: ["drawable-xxhdpi", "drawable-xxhdpi-landscape",
-        "drawable-xxhdpi-portrait", "drawable-xxxhdpi", "drawable-xhdpi",
-        "drawable-hdpi", "drawable-mdpi"
-    ]
-}), function indexResult(index) {
-    end = new Date();
-    console.log("Performance is: " + Number(end - start) + "ms.");
-    console.log(index);
-});
+var devices = {
+    iOS: new Device({
+        os: "iOS",
+        brandName: "iPhone 6S Plus"
+    }),
+    Android: new Device({
+        os: "Android",
+        brandName: "Note 3",
+        resourceFolderOrder: ["drawable-xxhdpi", "drawable-xxhdpi-landscape",
+            "drawable-xxhdpi-portrait", "drawable-xxxhdpi", "drawable-xhdpi",
+            "drawable-hdpi", "drawable-mdpi"
+        ]
+    })
+};
+
+
+var device = devices[argv[0]];
+ws.getIndex(device,
+    function indexResult(index) {
+        end = new Date();
+        console.log("Performance is: " + Number(end - start) + "ms.");
+        console.log(index);
+        process.exit(0);
+    });
